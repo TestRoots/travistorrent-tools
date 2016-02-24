7679bc32d9d652c48927a8b911a5a8a41e58ac39
Georgios Gousios <gousiosg@gmail.com>
2016-02-24 22:17:58 +0100

    Rework the harvester to collect undocumented response fields
    Specifically, the event_type field
    
    WIP

--- bin/travis_harvester.rb
+++ bin/travis_harvester.rb
@@ -2,80 +2,100 @@
 
 require 'travis'
 require 'net/http'
+require 'open-uri'
+require 'json'
 
 load 'lib/csv_helper.rb'
 
-def get_travis(repo)
+def job_logs(build, error_file, parent_dir)
+  jobs = build.jobs
+  jobs.each do |job|
+    name = File.join(parent_dir, "#{build.id}_#{build.commit.sha}_#{job.id.to_s}.log")
+    next if File.exists?(name)
+
+    begin
+      begin
+        log_url = "Attempt 1: http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job.id}/log.txt"
+        STDERR.puts log_url
+        log = Net::HTTP.get_response(URI.parse(log_url)).body
+      rescue
+        begin
+          # Give Travis CI some time before trying once more
+          STDERR.puts "Attempt 2 #{log_url}"
+          log = job.log.body
+        rescue
+          # Workaround if log.body results in error.
+          log_url = "http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job.id}/log.txt"
+          STDERR.puts "Attempt 3 #{log_url}"
+          log = Net::HTTP.get_response(URI.parse(log_url)).body
+        end
+      end
+
+      File.open(name, 'w') { |f| f.puts log }
+      log = '' # necessary to enable GC of previously stored value, otherwise: memory leak
+    rescue
+      error_message = "Could not get log #{name}"
+      puts error_message
+      File.open(error_file, 'a') { |f| f.puts error_message }
+      next
+    end
+  end
+end
+
+def get_travis(repo, build_logs = true)
   parent_dir = File.join('build_logs', repo.gsub(/\//, '@'))
-  save_file = File.join(parent_dir, 'repo-data-travis.csv')
   error_file = File.join(parent_dir, 'errors')
   FileUtils::mkdir_p(parent_dir)
+  all_builds = []
 
   begin
     repository = Travis::Repository.find(repo)
 
     puts "Harvesting Travis build logs for #{repo}"
-    highest_build = repository.last_build_number
-    repository.each_build do |build|
-      begin
-        jobs = build.jobs
-        jobs.each do |job|
-          name = File.join(parent_dir, build.id + '_' + build.commit.sha + '_' + job.id.to_s + '.log')
-          next if File.exists?(name)
+    highest_build = repository.last_build_number.to_i
+    while true do
+      highest_build = highest_build + 1
+      if highest_build % 25 == 0
+        break
+      end
+    end
 
-          begin
-            begin
-              log_url = "http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job.id}/log.txt"
-              log = Net::HTTP.get_response(URI.parse(log_url)).body
-            rescue
-              begin
-                # Give Travis CI some time before trying once more
-                log = job.log.body
-              rescue
-                # Workaround if log.body results in error.
-                log_url = "http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job.id}/log.txt"
-                log = Net::HTTP.get_response(URI.parse(log_url)).body
-              end
-            end
+    repo_id = JSON.parse(open("https://api.travis-ci.org/repos/#{repo}").read)['id']
 
-            File.open(name, 'w') { |f| f.puts log }
-            log = '' # necessary to enable GC of previously stored value, otherwise: memory leak
-          rescue
-            error_message = "Could not get log #{name}"
-            puts error_message
-            File.open(error_file, 'a') { |f| f.puts error_message }
-            next
-          end
+    (0..highest_build).select{|x| x % 25 == 0}.reverse_each do |last_build|
+
+      url = "https://api.travis-ci.org/builds?after_number=#{last_build}&repository_id=#{repo_id}"
+      STDERR.puts url
+
+      resp = open(url,
+                  'Content-Type' => 'application/json',
+                  'Accept' => 'application/vnd.travis-ci.2+json')
+      builds = JSON.parse(resp.read)
+      builds['builds'].each do |build|
+        begin
+          job_logs(build, error_file, parent_dir) if build_logs
+          commit = builds['commits'].find{|x| x['id'] == build['commit_id']}
+
+          build_data = {
+              :build_id    => build['id'],
+              :commit      => commit['sha'],
+              :pull_req    => build['pull_request_number'],
+              :branch      => commit['branch'],
+              :status      => build['state'],
+              :duration    => build['duration'],
+              :started_at  => build['started_at'],
+              :jobs        => build['job_ids'],
+              #:jobduration => build.jobs.map { |x| "#{x.id}##{x.duration}" }
+              :event_type  => build['event_type']
+          }
+
+          next if build_data.empty?
+          all_builds << build_data
+        rescue Exception => e
+          error_message = "Error getting Travis builds for #{repo} #{build['id']}: #{e.message}"
+          puts error_message
+          File.open(error_file, 'a') { |f| f.puts error_message }
         end
-
-        keys = ['build_id', 'commit', 'pull_req', 'branch', 'status', 'duration', 'started_at', 'jobs', 'jobduration']
-        values = [
-            build.id,
-            build.commit.sha,
-            build.pull_request_number,
-            build.commit.branch,
-            build.state,
-            build.duration,
-            build.started_at,
-            build.jobs.map { |x| x.id },
-            build.jobs.map { |x| "#{x.id}##{x.duration}"}
-        ]
-        build_data = Array.new
-        build_data << Hash[keys.zip values]
-
-        next if build_data.empty?
-
-        if !File.exists? save_file
-          # write out header
-          File.open(save_file, 'w') { |f| f.puts array_of_hashes_to_csv build_data }
-        else
-          # write in append mode without header
-          File.open(save_file, 'a') { |f| f.puts array_of_hashes_to_csv_without_header build_data }
-        end
-      rescue Exception => e
-        error_message = "Error getting Travis builds for #{repo} #{build.id}: #{e.message}"
-        puts error_message
-        File.open(error_file, 'a') { |f| f.puts error_message }
       end
     end
   rescue Exception => e
@@ -83,8 +103,22 @@ def get_travis(repo)
     puts error_message
     File.open(error_file, 'a') { |f| f.puts error_message }
   end
-end
 
+  # Remove duplicates
+  all_builds = all_builds.group_by{|x| x[:build_id]}.map{|k,v| v[0]}
+
+  json_file = File.join(parent_dir, 'repo-data-travis.json')
+  File.open(json_file, 'w') do |f|
+    f.puts JSON.dump(all_builds)
+  end
+
+  csv_file = File.join(parent_dir, 'repo-data-travis.csv')
+  File.open(csv_file, 'w') do |f|
+    f.puts all_builds.first.keys.map { |x| x.to_s }.join(',')
+    all_builds.sort { |a, b| b[:build_id]<=>a[:build_id] }.each { |x| f.puts x.values.join(',') }
+  end
+
+end
 
 if (ARGV[0].nil? || ARGV[1].nil?)
   puts 'Missing argument(s)!'
@@ -96,4 +130,4 @@ end
 owner = ARGV[0]
 repo = ARGV[1]
 
-get_travis("#{owner}/#{repo}")
+get_travis("#{owner}/#{repo}", false)
