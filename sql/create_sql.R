fe52461ca41bd0fa756d8b65da6602ae7dfcd159
Moritz Beller <moritzbeller@gmx.de>
2016-09-07 17:39:52 +0200

    Makes creation of database automatic

--- sql/create_sql.R
+++ sql/create_sql.R
@@ -1,6 +1,9 @@
 # Date time suport for date types only comes with RMySQL 11.3, which is in unstable currently
-devtools::install_github("rstats-db/RMySQL")
-devtools::install_github("rstats-db/DBI")
+# However, RMySQL 0.11.3 contains a bug that does not allow us to insert long entries in tr_tests_run
+# We hence use RMySQL 0.10.9 and manually modify the created SQL columns to DATETIME
+
+# Update to create new version of the data set
+table.name <- "travistorrent_8_9_2016"
 
 library(data.table)
 library(RMySQL)
@@ -18,7 +21,9 @@ data$tr_started_at <- as.POSIXct(data$tr_started_at)
 
 con <- dbConnect(dbDriver("MySQL"), user = "root", password = "root", dbname = "travistorrent", unix.socket='/var/run/mysqld/mysqld.sock')
 dbListTables(con)
-dbWriteTable(con, "travistorrent_7_9_2016", data, row.names = F, overwrite = T)
+dbWriteTable(con, table.name, data, row.names = F, overwrite = T)
+dbSendQuery(con, sprintf("ALTER TABLE %s MODIFY tr_started_at DATETIME;",table.name))
+dbSendQuery(con, sprintf("ALTER TABLE %s MODIFY gh_first_commit_created_at DATETIME;",table.name))
 dbDisconnect(con)
 
 # Manually assert transformation of data types
