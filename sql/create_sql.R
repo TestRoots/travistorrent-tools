4db51401f73a9ae2248a2cd16abee36793b9c744
Moritz Beller <moritzbeller@gmx.de>
2016-09-30 19:09:58 +0200

    Fix test run inconsistencies

--- sql/create_sql.R
+++ sql/create_sql.R
@@ -3,7 +3,7 @@
 # We hence use RMySQL 0.10.9 and manually modify the created SQL columns to DATETIME
 
 # Update to create new version of the data set
-table.name <- "travistorrent_8_9_2016"
+table.name <- "travistorrent_30_9_2016"
 
 library(data.table)
 library(RMySQL)
@@ -18,6 +18,17 @@ data$tr_tests_failed <- data$tr_tests_failed == "true"
 
 data$gh_first_commit_created_at <- as.POSIXct(data$gh_first_commit_created_at)
 data$tr_started_at <- as.POSIXct(data$tr_started_at)
+
+# Sanitize data runs with NAs instead of 0s
+data[tr_tests_failed == T & tr_tests_fail == 0,]$tr_tests_fail <- NA
+data[tr_tests_failed == T & tr_tests_run == 0,]$tr_tests_run <- NA
+data[tr_tests_ok < 0,]$tr_tests_ok <- NA
+data[tr_tests_fail > tr_tests_run,]$tr_tests_run <- NA
+
+# Sanitize previous builds as NA instead of -1
+data[tr_prev_build == -1,]$tr_prev_build <- NA
+
+csv.write(data, paste(table.name, "csv", sep="."))
 
 con <- dbConnect(dbDriver("MySQL"), user = "root", password = "root", dbname = "travistorrent", unix.socket='/var/run/mysqld/mysqld.sock')
 dbListTables(con)
