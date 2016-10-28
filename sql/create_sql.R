ff0c3b3211182fa048762780c89730a1f09c22b3
Moritz Beller <moritzbeller@gmx.de>
2016-10-28 23:11:06 +0200

    Convert previous build to integer

--- sql/create_sql.R
+++ sql/create_sql.R
@@ -26,6 +26,7 @@ data[tr_tests_ok < 0,]$tr_tests_ok <- NA
 data[tr_tests_fail > tr_tests_run,]$tr_tests_run <- NA
 
 # Sanitize previous builds as NA instead of -1
+data$tr_prev_build <- as.integer(data$tr_prev_build)
 data[tr_prev_build == -1,]$tr_prev_build <- NA
 
 write.csv(data, paste(table.name, "csv", sep="."), row.names = F)
