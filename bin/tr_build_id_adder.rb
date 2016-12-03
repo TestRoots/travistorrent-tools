9b3c424aadef27cbfa820726215e70a789ada7c7
Moritz Beller <moritzbeller@gmx.de>
2016-12-04 00:32:57 +0100

    Add retro-fit of build id

---
+++ bin/tr_build_id_adder.rb
@@ -0,0 +1,41 @@
+require 'csv'
+require 'travis'
+require 'parallel'
+
+load 'lib/csv_helper.rb'
+
+jobs = CSV.table('jobs.csv')
+
+i = 1
+length = jobs.length
+header = jobs.headers
+header.push :tr_build_id
+
+jobs = Parallel.map(jobs, in_threads: 10) do |job|
+  begin
+    job_id = job[2]
+    job.push Travis::Job.find(job_id).build_id
+
+    puts "#{i}/#{length}"
+    job
+  rescue Exception => e
+    STDERR.puts "Exception at #{i}"
+    STDERR.puts e
+  rescue Error => e
+    STDERR.puts "Error at #{i}"
+    STDERR.puts e
+  ensure
+    i += 1
+  end
+end
+
+jobs = jobs.select { |a| !a.nil? }
+
+CSV.open('jobs-n.csv', 'w',
+         :write_headers => true,
+         :headers => header
+) do |hdr|
+  jobs.each do |job|
+    hdr << job
+  end
+end
