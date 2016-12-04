5080161144aaff86061bbc34f86980806a4e1ba4
Moritz Beller <moritzbeller@gmx.de>
2016-12-04 14:07:06 +0100

    Implement greedy grabbing from Travis, with limit recovery

--- bin/tr_build_id_adder.rb
+++ bin/tr_build_id_adder.rb
@@ -14,21 +14,35 @@ length = jobs.length
 header = jobs.shift
 header.push 'tr_build_id'
 
-def get_build_id job_id
-   # used to be a simple Travis::Job.find(job_id).build_id job_id
+def get_build_id(job_id, wait_in_s)
+  # used to be a simple Travis::Job.find(job_id).build_id
 
   url = "https://api.travis-ci.org/jobs/#{job_id}"
-  resp = open(url,
-              'Content-Type' => 'application/json',
-              'Accept' => 'application/vnd.travis-ci.2+json')
-  json_resp = JSON.parse(resp.read)
-  json_resp['job']['build_id']
+  begin
+    resp = open(url,
+                'Content-Type' => 'application/json',
+                'Accept' => 'application/vnd.travis-ci.2+json')
+    json_resp = JSON.parse(resp.read)
+    return json_resp['job']['build_id']
+  rescue Exception => e
+    STDERR.puts "Exception at #{job_id}"
+    if e.io.status[0] == "429"
+      STDERR.puts "Encountered API restriction: sleeping for #{wait_in_s}"
+      sleep wait_in_s
+      return get_build_id job_id, wait_in_s*2
+    else
+      raise e
+    end
+
+  end
+
 end
 
-jobs = Parallel.map(jobs, in_threads: 10) do |job|
+jobs = Parallel.map(jobs, in_threads: 50) do |job|
   begin
     job_id = job[2]
-    build_id = get_build_id job_id
+    build_id = get_build_id job_id, 2
+    sleep 0.01
     puts "#{i}/#{length}"
     job.push build_id
   rescue Exception => e
