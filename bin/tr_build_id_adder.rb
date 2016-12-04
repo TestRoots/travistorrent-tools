89e621d913fcabaafbd31ec66b76af641f1f88b9
Moritz Beller <moritzbeller@gmx.de>
2016-12-04 10:40:11 +0100

    Add argument

--- bin/tr_build_id_adder.rb
+++ bin/tr_build_id_adder.rb
@@ -4,14 +4,14 @@ require 'parallel'
 
 load 'lib/csv_helper.rb'
 
-jobs = CSV.read('jobs.csv')
+jobs = CSV.read(ARGV[0])
 
 i = 1
 length = jobs.length
 header = jobs.shift
 header.push 'tr_build_id'
 
-jobs = Parallel.map(jobs, in_threads: 25) do |job|
+jobs = Parallel.map(jobs, in_threads: 2) do |job|
   begin
     job_id = job[2]
     job.push Travis::Job.find(job_id).build_id
@@ -31,7 +31,7 @@ end
 
 jobs = jobs.select { |a| !a.nil? }
 
-CSV.open('jobs-n.csv', 'w',
+CSV.open("n_#{ARGV[0]}", 'w',
          :write_headers => true,
          :headers => header
 ) do |hdr|
