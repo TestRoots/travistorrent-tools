8af8c8b0834e65f93ea8f1335f6183d7d382c66b
Georgios Gousios <gousiosg@gmail.com>
2016-12-21 22:17:19 +0100

    More tests

--- tests/go_test.rb
+++ tests/go_test.rb
@@ -33,29 +33,68 @@ describe GoData do
 
   end
 
-  describe 'when asked to recognize test cases' do
+  describe 'when asked to recognize cases' do
 
-    test_case_filter = Class.new.extend(GoData).test_case_filter
-
-    it 'must accept valid test function names' do
-      assert test_case_filter.call('func TestXxx(*testing.T)')
-      assert test_case_filter.call('func  TestXxx(*testing.T)')
-      assert test_case_filter.call('func TestXxx( *testing.T)')
-      assert test_case_filter.call('func TestXxx(*testing.T )')
-      assert test_case_filter.call('func TestΤεστ(*testing.T )')
-      assert test_case_filter.call('func TestΤεστ (*testing.T )')
-      assert test_case_filter.call('func Testτεστ(*testing.T)')
+    it 'must accept valid test gotest function names' do
+      assert Class.new.extend(GoData).is_gotest_test_case('func TestXxx(*testing.T)')
+      assert Class.new.extend(GoData).is_gotest_test_case('func  TestXxx(*testing.T)')
+      assert Class.new.extend(GoData).is_gotest_test_case('func TestXxx( *testing.T)')
+      assert Class.new.extend(GoData).is_gotest_test_case('func TestXxx(*testing.T )')
+      assert Class.new.extend(GoData).is_gotest_test_case('func TestΤεστ(*testing.T )')
+      assert Class.new.extend(GoData).is_gotest_test_case('func Test_Τεστ(*testing.T )')
+      assert Class.new.extend(GoData).is_gotest_test_case('func TestΤεστ (*testing.T )')
+      assert Class.new.extend(GoData).is_gotest_test_case('func Testτεστ(*testing.T)')
 
     end
 
-    it 'must reject invalid test function names' do
-      assert (not test_case_filter.call('func Test(*testing.T)'))
-      assert (not test_case_filter.call('func ΤestFoo(*testing.T)'))
-      assert (not test_case_filter.call('func testFoo(*testing.T)'))
-      assert (not test_case_filter.call("func testFoo(*testing.T\n)"))
+    it 'must reject invalid gotest function names' do
+      assert (not Class.new.extend(GoData).is_gotest_test_case('func Test(*testing.T)'))
+      assert (not Class.new.extend(GoData).is_gotest_test_case('func ΤestFoo(*testing.T)'))
+      assert (not Class.new.extend(GoData).is_gotest_test_case('func testFoo(*testing.T)'))
+      assert (not Class.new.extend(GoData).is_gotest_test_case("func testFoo(*testing.T\n)"))
+    end
+
+    it 'must accept valid GoConvey test cases' do
+      assert Class.new.extend(GoData).is_GoConvey_test_case('Convey("When calling SyncSignedInUser", t, func()')
+      assert Class.new.extend(GoData).is_GoConvey_test_case('Convey ("Should remove org role", func()')
+      assert Class.new.extend(GoData).is_GoConvey_test_case('Convey("Should add cert", func() {')
     end
 
   end
 
+  describe 'when asked to recognize assertions' do
+
+    it 'must accept valid gotest assertions' do
+      assert Class.new.extend(GoData).is_gotest_assertion('t.Fatalf("expected to allocate between 1 and 2")')
+      assert Class.new.extend(GoData).is_gotest_assertion('Fatal("expected to allocate between 1 and 2")')
+      assert Class.new.extend(GoData).is_gotest_assertion('b.Errorf("unable to unmarshal nanosecond data: %s", err.Error())')
+      assert Class.new.extend(GoData).is_gotest_assertion('Errorf ("expected to allocate between 1 and 2")')
+
+    end
+
+    it 'must accept valid GoConvey assertions' do
+      assert Class.new.extend(GoData).is_GoConvey_assertion('So(transport.TLSClientConfig.InsecureSkipVerify, ShouldEqual, false)')
+      assert Class.new.extend(GoData).is_GoConvey_assertion('So(len(transport.TLSClientConfig.Certificates),ShouldEqual, 0)')
+      assert Class.new.extend(GoData).is_GoConvey_assertion('So((transport.TLSClientConfig.Certificates),ShouldEqual, 0)')
+      assert Class.new.extend(GoData).is_GoConvey_assertion('So(rule1.ContainsUpdates(rule2), ShouldBeFalse)')
+      assert Class.new.extend(GoData).is_GoConvey_assertion('So(rule1.ContainsUpdates(rule2),ShouldBeFalse)')
+    end
+
+    it 'must accept valid testify assertions' do
+      assert Class.new.extend(GoData).is_testify_assertion('assert.NoError(t, node1.AddContainer(createContainer("c1", config)))')
+      assert Class.new.extend(GoData).is_testify_assertion('assert.Equal(t, node1.UsedCpus, int64(1))')
+      assert Class.new.extend(GoData).is_testify_assertion('Assert.Panics(t,node2.AddContainer(createContainer("c2", config)))')
+      assert Class.new.extend(GoData).is_testify_assertion('a.NotEqual(t, node2.UsedCpus, int64(1))')
+      assert Class.new.extend(GoData).is_testify_assertion('assert.Nil(t, containers.Get("invalid-id"))')
+
+    end
+
+    it 'must accept valid assertions' do
+      assert Class.new.extend(GoData).assertion_filter.call('So(transport.TLSClientConfig.InsecureSkipVerify, ShouldEqual, false)')
+      assert Class.new.extend(GoData).assertion_filter.call('Errorf ("expected to allocate between 1 and 2")')
+      assert Class.new.extend(GoData).is_testify_assertion('assert.Nil(t, containers.Get("invalid-id"))')
+    end
+
+  end
 
 end
