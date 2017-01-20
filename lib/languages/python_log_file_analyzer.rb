41affb0f20d77864baaaf7ac894c3c443843c039
Moritz Beller <moritzbeller@gmx.de>
2017-01-20 11:49:08 +0100

    Python: Add initial pytest detector

--- lib/languages/python_log_file_analyzer.rb
+++ lib/languages/python_log_file_analyzer.rb
@@ -1,4 +1,4 @@
-# A Mixin for the analysis of Python build files. Supports unittest, tox
+# A Mixin for the analysis of Python build files. Supports unittest, tox, pytest (WIP)
 
 module PythonLogFileAnalyzer
   attr_reader :tests_failed, :test_duration, :reactor_lines, :pure_build_duration
@@ -41,13 +41,29 @@ module PythonLogFileAnalyzer
     end
   end
 
+  def analyze_pytest_status_info_list(string)
+    additional_information = string.split(', ')
+    additional_information.each do |arg|
+      arg.split(' ').each_cons(2) do |val, key|
+        # TODO: we could add xpassed syntax here
+        case key.downcase
+          when 'passed'
+            @num_tests_run += val.to_i
+          when 'failed'
+            @num_tests_failed += val.to_i
+            @num_tests_runt += val.to_i
+        end
+      end
+    end
+  end
+
   def analyze_status_info_list(string)
     additional_information = string.split(', ')
     additional_information.each do |arg|
       arg.split('=').each_cons(2) do |key, val|
         # TODO: we could add xpassed syntax here
         case key.downcase
-          when 'skip'
+        when 'skip'
             @num_tests_skipped = val.to_i
           when 'errors', 'failures', 'error', 'failure'
             @num_tests_failed += val.to_i
@@ -61,12 +77,18 @@ module PythonLogFileAnalyzer
 
     @test_lines.each do |line|
       if !(line =~ /Ran (\d+) tests? in (.+s)/).nil?
-        # Matches the test summary, i.e. "Ran 3 tests in 0.000s"
+        # Matches the testunit test summary, i.e. "Ran 3 tests in 0.000s"
         setup_python_tests
         add_framework 'unittest'
         @num_tests_run = $1.to_i
         @test_duration += convert_plain_time_to_seconds $2
         @has_summary = true
+      elsif !(line =~ /==================== (.+) in (.+) seconds ====================/).nil?
+        # Matches the pytest test summary, i.e. "==================== 442 passed, 2 xpassed in 50.65 seconds ===================="
+        setup_python_tests
+        add_framework 'pytest'
+        analyze_pytest_status_info_list $1
+        @test_duration += $2.to_f
       elsif !(line =~ /^OK( \((.+)\))?\s*$/).nil? and @has_summary
         # This is a somewhat dangerous thing to do as "OK" might be a common line in builds. We mititgate the risk somewhat by having seen a summary
         # TODO we can make this more clever by checking only AFTER a summary
@@ -87,7 +109,7 @@ module PythonLogFileAnalyzer
         # TODO: clear stuff ala "py27: commands failed" from it
         # Matches the likes of FAIL: test_em (__main__.TestMarkdownPy)
         setup_python_tests
-        add_failed_test $4 # TODO use member method everywhere
+        add_failed_test $4
       elsif !(line =~ /^FAIL: (\S+)/).nil? and !summary_seen
         # Matches the likes of FAIL: test_em (__main__.TestMarkdownPy)
         setup_python_tests
@@ -99,7 +121,11 @@ module PythonLogFileAnalyzer
   end
 
   def tests_failed?
-    return !@force_tests_passed || !@tests_failed.empty? || (!@num_tests_failed.nil? && @num_tests_failed > 0)
+    if @force_tests_passed
+      return false
+    end
+
+    return !@tests_failed.empty? || (!@num_tests_failed.nil? && @num_tests_failed > 0)
   end
 
 end
