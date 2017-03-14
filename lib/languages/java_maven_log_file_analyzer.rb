e87c7be34cae99c30be6e3da8287ed44ac695c4e
Suleman Mahmood <msm6@illinois.edu>
2017-03-13 22:23:20 -0500

    covers many new cases

--- lib/languages/java_maven_log_file_analyzer.rb
+++ lib/languages/java_maven_log_file_analyzer.rb
@@ -13,7 +13,7 @@ module JavaMavenLogFileAnalyzer
     @tests_failed_lines = Array.new
     @tests_failed = Array.new
     @analyzer = 'java-maven'
-    @processFinalSection = false
+    @processFinalSection = true
     @FailedTestProcessing = false
     @TestInErrorProcessing = false
   end
@@ -95,27 +95,61 @@ module JavaMavenLogFileAnalyzer
   end
 
   def extractTestNameAndMethod(string)
-    return "" if (string.nil?) or not (string.include? "(")
+    return "" if (string.nil?)
     
-    # this is Test in Error processing
     if @FailedTestProcessing
-#      puts "input string is : #{string}"
-      # this case happens when line starts with Failed tests so there may be two cases here
-      testClass = string.split('(')
-      testName = testClass[0].split[2].strip
-      testName = testClass[1].split(')')[0] << '.' << testName
-      return testName
+      headerstring = "Failed tests:"
     elsif @TestInErrorProcessing
-      testClass = string.split('(')
-      testName = testClass[0].strip
-      testName = testClass[1].split(')')[0] << '.' << testName
-      return testName
+      headerstring = "Tests in error:"
     end
+    
+    if string.strip == headerstring
+      return ""
+    elsif string.start_with?(headerstring)
+      string = string.split(":")[1]
+    else
+      string = string.split(":")[0]
+    end
+    
+    if (string =~ /^  [a-zA-Z0-9\.\_]/).nil? and not(string.start_with?("com.") )
+      return ""
+    end
+    
+    if (string.start_with?("  Could not initialize class")) 
+      return ""
+    end
+
+    if string.start_with?("  Run ")
+  #    puts "string is : ___________#{string}_________"
+      return ""
+    end
+    
+    if string.include? "(" 
+      testClass = string.split('(')
+      testName = testClass[0].split[0].strip
+      testClass = testClass[1].split(')')[0]
+      if not(testName.start_with?(testClass))
+        testName = testClass << '.' << testName
+      end
+    else      
+      testName = string.split(":")[0].strip
+    end
+    
+    if testName.include? "["
+      testName = testName.split("[")[0]
+    end
+    
+    if testName.include? " "
+      testName = testName.split(" ")[0]
+    end
+    
+    return testName
   end
 
   def analyze_tests
     failed_tests_started = false
     has_tests_run_per_testClass = false
+    numberoffailures = 0
     
     index = 0
     
@@ -123,21 +157,39 @@ module JavaMavenLogFileAnalyzer
       if failed_tests_started
         @tests_failed_lines << line
         if line.strip.empty?
-          failed_tests_started = false
+          if @test_lines[index+1].strip.empty?
+            failed_tests_started = false
+          end
         end
       end
       if !(line =~ /Tests run: (\d*), Failures: (\d*), Errors: (\d*)(, Skipped: (\d*))?, Time elapsed: (.* sec) (<<< FAILURE!)?/).nil?
-        has_tests_run_per_testClass = true
         numberoffailures = $2.to_i + $3.to_i
-        
-        if (numberoffailures != 0)
-          if !(@test_lines[index + 1] =~ /Running ([a-zA-Z0-9\.]*)/ ).nil?
-            @processFinalSection = true
-          end
+        if numberoffailures > 0
+          has_tests_run_per_testClass = true
+        else
+          has_tests_run_per_testClass = false
         end
-          
-      elsif has_tests_run_per_testClass and !(line =~ /([a-zA-Z0-9\.]+)\(([^\)]+)\)\s+Time elapsed/).nil?
+      elsif has_tests_run_per_testClass and !(line =~ /([a-zA-Z0-9\.\_]+)\(([^\)]+)\)\s+Time elapsed/).nil?
         @tests_failed <<  ($2<<"."<<$1)
+        @processFinalSection = false
+        numberoffailures = numberoffailures - 1
+        if numberoffailures == 0
+          has_tests_run_per_testClass = false
+        end
+      elsif has_tests_run_per_testClass and !(line =~ /([a-zA-Z0-9\.\_]+)\[([a-zA-Z0-9\.\_\:]+)\]\(([^\)]+)\)\s+Time elapsed/).nil?
+        @tests_failed <<  ($3<<"."<<$1)
+        @processFinalSection = false
+        numberoffailures = numberoffailures - 1
+        if numberoffailures == 0
+          has_tests_run_per_testClass = false
+        end
+      elsif has_tests_run_per_testClass and !(line =~ /([a-zA-Z0-9\.\_]+)\s+Time elapsed/).nil?
+        @tests_failed <<  $1
+        @processFinalSection = false
+        numberoffailures = numberoffailures - 1
+        if numberoffailures == 0
+          has_tests_run_per_testClass = false
+        end
       elsif !has_tests_run_per_testClass and !(line =~ /Tests run: .*? Time elapsed: (.* sec)/).nil?
         init_tests
         @tests_run = true
