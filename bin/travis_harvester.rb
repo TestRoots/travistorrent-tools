d20f6e8f2cad85e77ddf4660fcf02f7388f4fd3a
Jackson Antonio do Prado Lima <jacksonpradolima@gmail.com>
2018-11-06 10:06:58 +0000

    fix s3 access denied - using API Travis v3 to get log

--- bin/travis_harvester.rb
+++ bin/travis_harvester.rb
@@ -33,13 +33,12 @@ def download_job(job, name, wait_in_s = 1)
   end
 
   begin
+    log_url = "https://api.travis-ci.org/v3/job/#{job}/log.txt"
     begin
-      log_url = "http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job}/log.txt"
       STDERR.puts "Attempt 1 #{log_url}"
       log = Net::HTTP.get_response(URI.parse(log_url)).body
     rescue
-      # Workaround if log.body results in error.
-      log_url = "http://s3.amazonaws.com/archive.travis-ci.org/jobs/#{job}/log.txt"
+      # Workaround if log.body results in error
       STDERR.puts "Attempt 2 #{log_url}"
       log = Net::HTTP.get_response(URI.parse(log_url)).body
     end
@@ -91,22 +90,22 @@ def get_build(builds, build, wait_in_s = 1)
     job_logs(build, commit['sha']) if @build_logs
 
     build_data = {
-        :build_id => build['id'],
-        :commit => commit['sha'],
-        :pull_req => build['pull_request_number'],
-        :branch => commit['branch'],
-        # [doc] The build status (such as passed, failed, ...) as returned from the Travis CI API.
-        :status => build['state'],
+      :build_id => build['id'],
+      :commit => commit['sha'],
+      :pull_req => build['pull_request_number'],
+      :branch => commit['branch'],
+      # [doc] The build status (such as passed, failed, ...) as returned from the Travis CI API.
+      :status => build['state'],
 
-        # [doc] The full build duration as returned from the Travis CI API.
-        :duration => build['duration'],
-        :started_at => started_at, # in UTC
+      # [doc] The full build duration as returned from the Travis CI API.
+      :duration => build['duration'],
+      :started_at => started_at, # in UTC
 
-        # [doc] The unique Travis IDs of the jobs, in a string separated by `#`.
-        :jobs => build['job_ids'],
+      # [doc] The unique Travis IDs of the jobs, in a string separated by `#`.
+      :jobs => build['job_ids'],
 
-        #:jobduration => build.jobs.map { |x| "#{x.id}##{x.duration}" }
-        :event_type => build['event_type']
+      #:jobduration => build.jobs.map { |x| "#{x.id}##{x.duration}" }
+      :event_type => build['event_type']
     }
 
     return build_data
@@ -173,58 +172,58 @@ def get_travis(repo, build_logs = true, wait_in_s = 1)
     highest_build = repository.last_build_number.to_i
     puts "Harvesting Travis build logs for #{repo} (#{highest_build} builds)"
     while true do
-      highest_build = highest_build + 1
-      if highest_build % 25 == 0
-        break
+        highest_build = highest_build + 1
+        if highest_build % 25 == 0
+          break
+        end
       end
+
+      repo_id = JSON.parse(open("https://api.travis-ci.org/repos/#{repo}").read)['id']
+
+      (0..highest_build).select { |x| x % 25 == 0 }.reverse_each do |last_build|
+        all_builds << paginate_build(last_build, repo_id)
+      end
+    rescue Exception => e
+      error_message = "Retrying, but Error getting Travis builds for #{repo}: #{e.message}"
+      puts error_message
+      File.open(@error_file, 'a') { |f| f.puts error_message }
+      get_travis(repo, build_logs, wait_in_s*2)
+      return
     end
 
-    repo_id = JSON.parse(open("https://api.travis-ci.org/repos/#{repo}").read)['id']
+    all_builds.flatten!
+    # Remove empty entries
+    all_builds.reject! { |c| c.empty? }
+    # Remove duplicates
+    all_builds = all_builds.group_by { |x| x[:build_id] }.map { |k, v| v[0] }
 
-    (0..highest_build).select { |x| x % 25 == 0 }.reverse_each do |last_build|
-      all_builds << paginate_build(last_build, repo_id)
+    if all_builds.empty?
+      error_message = "Error could not get any repo information for #{repo}."
+      puts error_message
+      File.open(@error_file, 'a') { |f| f.puts error_message }
+      exit(1)
     end
-  rescue Exception => e
-    error_message = "Retrying, but Error getting Travis builds for #{repo}: #{e.message}"
-    puts error_message
-    File.open(@error_file, 'a') { |f| f.puts error_message }
-    get_travis(repo, build_logs, wait_in_s*2)
-    return
+
+    File.open(json_file, 'w') do |f|
+      f.puts JSON.dump(all_builds)
+    end
+
+    csv_file = File.join(@parent_dir, 'repo-data-travis.csv')
+    File.open(csv_file, 'w') do |f|
+      f.puts all_builds.first.keys.map { |x| x.to_s }.join(',')
+      all_builds.sort { |a, b| b[:build_id]<=>a[:build_id] }.each { |x| f.puts x.values.join(',') }
+    end
+
   end
 
-  all_builds.flatten!
-  # Remove empty entries
-  all_builds.reject! { |c| c.empty? }
-  # Remove duplicates
-  all_builds = all_builds.group_by { |x| x[:build_id] }.map { |k, v| v[0] }
-
-  if all_builds.empty?
-    error_message = "Error could not get any repo information for #{repo}."
-    puts error_message
-    File.open(@error_file, 'a') { |f| f.puts error_message }
+  if (ARGV[0].nil? || ARGV[1].nil?)
+    puts 'Missing argument(s)!'
+    puts ''
+    puts 'usage: travis_harvester.rb owner repo'
     exit(1)
   end
 
-  File.open(json_file, 'w') do |f|
-    f.puts JSON.dump(all_builds)
-  end
+  owner = ARGV[0]
+  repo = ARGV[1]
 
-  csv_file = File.join(@parent_dir, 'repo-data-travis.csv')
-  File.open(csv_file, 'w') do |f|
-    f.puts all_builds.first.keys.map { |x| x.to_s }.join(',')
-    all_builds.sort { |a, b| b[:build_id]<=>a[:build_id] }.each { |x| f.puts x.values.join(',') }
-  end
-
-end
-
-if (ARGV[0].nil? || ARGV[1].nil?)
-  puts 'Missing argument(s)!'
-  puts ''
-  puts 'usage: travis_harvester.rb owner repo'
-  exit(1)
-end
-
-owner = ARGV[0]
-repo = ARGV[1]
-
-get_travis("#{owner}/#{repo}", true)
+  get_travis("#{owner}/#{repo}", true)
