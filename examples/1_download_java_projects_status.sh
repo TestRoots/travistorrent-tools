ce69425edc7cea40cdfa672a1809a0d117a47c78
Jackson Antonio do Prado Lima <jacksonpradolima@gmail.com>
2018-11-07 17:38:19 +0000

    update examples

--- examples/1_download_java_projects_status.sh
+++ examples/1_download_java_projects_status.sh
@@ -4,6 +4,6 @@ printf "If you have difficult to execute this script, please, read this link: ht
 dir=`pwd`
 
 printf "Connecting to GHTorrent and executing a query to extract main Java projects from GitHub...\n"
-mysql -u ght -h 127.0.0.1 ghtorrent < $dir/examples/java_projects.sql | sed 's/\t/;/g' > $dir/java_projects.txt
+mysql -u ght -h 127.0.0.1 ghtorrent < $dir/examples/java_projects.sql | sed 's/\t/;/g' > $dir/gh-active-java-projects.csv
 
 printf "Java projects collected!\n"
--- examples/2_run_travis_poker.sh
+++ examples/2_run_travis_poker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ..
-printf "If you have difficult to execute this script, please, read this link: http://ghtorrent.org/mysql.html\n\n"
+printf "Now, we pokes en-mass whether a project has a Travis build history\n\n"
 dir=`pwd`
 
-# ruby -Ibin bin/travis_poker.rb -s DSpace/DSpace -t 2013-07-25
-
+ruby -Ibin bin/travis_poker.rb -f $dir/gh-active-java-projects.csv
